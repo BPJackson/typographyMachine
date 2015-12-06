96f275a42090dbb81c300a4cd1b9a9371925a703
TonyMcKendry <TonyMcKendry@gmail.com>
2015-12-05 20:41:36 -0700

    button now clears page to make room for typographized text

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log("it works");
+$("#button").click(function(){
+  var userInput = document.getElementById('userInput').value;
+  console.log(userInput);
+  $("header").remove();
+  $("#inputArea").remove();
+})
