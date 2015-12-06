4faafa56bee6e3e8e26e066c35c9a4ed972b19ed
TonyMcKendry <TonyMcKendry@gmail.com>
2015-12-05 21:22:11 -0700

    page is cleared and word is added once button is clicked

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
+var userInput = "";
+
 $("#button").click(function(){
-  var userInput = document.getElementById('userInput').value;
+  userInput = document.getElementById('userInput').value;
   console.log(userInput);
   $("header").remove();
   $("#inputArea").remove();
+  makeWordSpan(userInput);
 })
+
+makeWordSpan = function(userInput){
+  $("main").append("<div id='wordSpan'>" + userInput + "</div>")
+}
