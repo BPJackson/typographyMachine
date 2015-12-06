c2f15d051361a8d9390ed193bebfb0696fe85889
TonyMcKendry <TonyMcKendry@gmail.com>
2015-12-05 23:11:22 -0700

    initial css styles are applied to inputted text and background

--- js/app.js
+++ js/app.js
@@ -6,8 +6,32 @@ $("#button").click(function(){
   $("header").remove();
   $("#inputArea").remove();
   makeWordSpan(userInput);
-})
+  taseMeBro();
+});
 
 makeWordSpan = function(userInput){
-  $("main").append("<div id='wordSpan'>" + userInput + "</div>")
+  $("main").append("<div id='wordDiv'><span id='wordSpan'>" + userInput + "</span></div>")
+}
+
+$("#resetButton").click(function(){
+  location.reload();
+});
+
+
+$("#resetButton").mouseover(function(){
+  $(this).attr('src', 'images/reset-icon-neg.png');
+});
+
+$("#resetButton").mouseout(function(){
+  $(this).attr('src', 'images/reset-icon.png');
+  });
+
+taseMeBro = function(){
+  var fontvar = 'arial';
+  $(document).ready(function(){
+  $("#wordSpan").addClass("raleway");
+  $("body").css('background', "linear-gradient(to right, #1e5799 0%,#2989d8 100%)");
+  $("body").css('color', "white");
+
+  })
 }
