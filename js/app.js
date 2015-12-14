fa6f942737a26c3efe95147a264898fe900c1555
TonyMcKendry <TonyMcKendry@gmail.com>
2015-12-14 11:23:25 -0700

    flowers bebas style added

--- js/app.js
+++ js/app.js
@@ -14,16 +14,20 @@ $(document).on("click", "#button", function(){
   $("header").remove();
   $("#inputArea").remove();
   makeWordSpan(userInput);
-    rand = Math.random() * 100;
-  if (rand < 33){
-    earlyNight();
-  }
-  else if (rand > 33 && rand < 66){
-    taseMeBro();
-  }
-  else if (rand > 66){
-    beautyWords();
-  }
+  flowers();
+  //   rand = Math.random() * 100;
+  // if (rand < 25){
+  //   earlyNight();
+  // }
+  // else if (rand > 25 && rand < 50){
+  //   taseMeBro();
+  // }
+  // else if (rand > 50 && rand < 75){
+  //   flowers();
+  // }
+  // else if (rand > 75){
+  //   beautyWords();
+  // }
   jQuery(".fit").fitText();
 })
 
@@ -65,4 +69,12 @@ beautyWords = function(){
   $("body").css('background', "linear-gradient(to left, #d7d5d5 0%,#ededed 100%)");
   $("#wordDiv").prepend("<img id='blockquote' src='http://typespiration.com/wp-content/themes/typespiration/images/quote.png'><br>");
   $('#wordDiv').css("margin-top","-100px");
+}
+
+flowers = function(){
+  $("#wordSpan").addClass("flowers");
+  $("main").prepend("<img id=flowers src='images/flowers.jpg'>");
+  $("#wordDiv").css("width", "80vw");
+  $("#wordDiv").css("padding-left", "10vw");
+
 }
